a5532de9b94aa63f7cfc6f28d6ede2e648f253bb
alwathan <jazuli.gh@gmail.com>
2022-02-21 12:45:04 +0700

    first commit

---
+++ src/HelloWordl.vue
@@ -0,0 +1,58 @@
+<template>
+    <h1>Hello World!</h1>
+     <h4>Project Vue Pertama Saya</h4>
+    <h4>Nama Saya {{ nama }}</h4>
+    <h4>Umur Saya {{ umur }} tahun</h4>
+    <hr>
+    <h4>{{ bilangan }}</h4>
+    <button v-on:click="next">Next</button>
+    <hr>
+    <h4>{{ messages }}</h4>
+    <input v-model="messages" />
+    <hr>
+    <h4 v-if="ganjil">Ganjil</h4>
+    <h4 v-if="genap">Genap</h4>
+    <button v-on:click="randomNumber">Generate</button>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            nama : "Ach jazuli",
+            umur    : 35,
+            bilangan : 1,
+            messages : '',
+            ganjil : false,
+            genap : false,
+        }
+    },
+    mounted(){
+        this.umur = this.umur + 5;
+        this.randomNumber();
+    },
+    methods:{
+        next(){
+            this.bilangan += 1;
+            this.showHide();
+        },
+        randomNumber(){
+            this.bilangan = Math.floor(Math.random()*100);
+            this.showHide();
+        },
+        showHide(){
+            if(this.bilangan%2==0){
+                this.genap = true;
+                this.ganjil = false;
+            }else{
+                this.genap = false;
+                this.ganjil = true; 
+            }
+        }
+    } 
+}
+</script>
+
+<style scoped>
+
+</style>
